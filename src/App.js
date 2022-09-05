ced2a343b5620027bbf7a3fdc5d3db64eb0f47b5
Austin Perrine <akperrine@gmail.com>
2022-09-05 10:52:53 -0600

    First commit:
    Connect to Firestore DB and be able to update workout field with a workout array, given a collection ID with a valid user.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  query,
+  where,
+  onSnapshot,
+  getDoc,
+  updateDoc,
+  doc,
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBYwcfG5kErnzPDeDOjtIlE-g698jwltVM",
+  authDomain: "fitness-app-1e36b.firebaseapp.com",
+  projectId: "fitness-app-1e36b",
+  storageBucket: "fitness-app-1e36b.appspot.com",
+  messagingSenderId: "604488646893",
+  appId: "1:604488646893:web:521903a0c8db441b9a4ce4",
+  measurementId: "G-MG7P82E8BV",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 function App() {
+  const [idSearchField, setIdSearchField] = React.useState("");
+
+  const handleChange = (e) => {
+    setIdSearchField(e.target.value);
+  };
+
+  const handleUpdate = async (strCode) => {
+    const workoutArr = [
+      {
+        day: 1,
+        workout: [
+          "3rds",
+          "A: 6-7 front squats 18lbs bar (2020)",
+          "A: 5 dumbell Benchpress 10lb DB (2110)",
+          "3rds:",
+          "B: 6-7 Romanian Deadlifts 10lb DB (2011)",
+          "B: 6-7 single arm DB Row 10 (2020)",
+          "4 Rds:",
+          "1min spin on the bike (check what watts or RPMs fit you well)",
+          "20-30s dead bug hold",
+        ],
+        complete: false,
+      },
+      { day: 2, workout: [], complete: false },
+      {
+        day: 3,
+        workout: [
+          "3rds",
+          "A: 5-7 pushups to a bench (2011)",
+          "A: 6-10 scap pull-ups (2120)",
+        ],
+        complete: false,
+      },
+      { day: 4, workout: [], complete: false },
+      { day: 5, workout: [], complete: false },
+      {
+        day: 6,
+        workout: [
+          "Steady state day:",
+          "20min: ",
+          "-fast walk or jog (should be easy) down to Munsils and back. (We should start measuring some stuff out)",
+          "-12 no push-up burpees (be a slow pace person, take reps between.",
+          "-30 sec plank",
+        ],
+        complete: false,
+      },
+      { day: 7, workout: [], complete: false },
+    ];
+
+    const docRef = doc(db, "users", strCode);
+
+    await updateDoc(docRef, {
+      workout: workoutArr,
+    })
+      .then(() => alert("update successful"))
+      .catch((err) => alert(`unsucessful, error:${err}`));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Welcome Austin</h2>
+      <input
+        type="text"
+        placeholder="user ID"
+        value={idSearchField}
+        onChange={handleChange}
+      />
+      <button onClick={handleUpdate(idSearchField)}>
+        Update Client Workouts
+      </button>
     </div>
   );
 }
