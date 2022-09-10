c89e9abb252ca6d777e392748dd7add4612c8cf3
Austin Perrine <akperrine@gmail.com>
2022-09-10 17:14:43 -0600

    add input form functionallity to update daily exercise on btn press with excerpts and ids

--- src/components/WorkoutDay/workoutDay.jsx
+++ src/components/WorkoutDay/workoutDay.jsx
@@ -4,6 +4,7 @@ import React from "react";
 const WorkoutDay = ({ dayIndex }) => {
   const [exerciseArr, setExerciseArr] = React.useState([]);
   const [dayInputField, setDayInputField] = React.useState("");
+  console.log(dayInputField, exerciseArr);
 
   const weekdays = [
     "Sunday",
@@ -14,18 +15,38 @@ const WorkoutDay = ({ dayIndex }) => {
     "Friday",
     "Saturday",
   ];
+
+  const handleAddNote = (event) => {
+    event.preventDefault();
+    const newArray = [...exerciseArr];
+    newArray.push({ excerpt: dayInputField, id: Date.now() });
+    setExerciseArr(newArray);
+  };
+
+  const handleInputChange = (event) => {
+    event.preventDefault();
+    setDayInputField(event.target.value);
+  };
+
   return (
     <div>
       <h4>Workout For {weekdays[dayIndex]}</h4>
       {exerciseArr.length ? (
         exerciseArr.map((note) => {
-          return <p>{note}</p>;
+          return <p>{note.excerpt}</p>;
         })
       ) : (
         <p>Nothing currently Programmed</p>
       )}
-      <label>Add a workout Piece</label>
-      <input />
+      <form onSubmit={handleAddNote}>
+        <label>Add a workout Piece</label>
+        <input
+          name="exercise-input"
+          placeholder="add an exercise or note"
+          onChange={handleInputChange}
+        />
+        <button>Add Exercise note</button>
+      </form>
     </div>
   );
 };
