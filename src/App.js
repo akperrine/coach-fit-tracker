b4708a21a0baa477d8b7edbd3acb2034f0cbd851
Austin Perrine <akperrine@gmail.com>
2022-09-11 20:49:44 -0600

    Add Link with props of client data to ensure proper user is updated

--- src/App.js
+++ src/App.js
@@ -1,87 +1,14 @@
 import "./App.css";
-import React, { useEffect } from "react";
-import WorkoutDay from "./components/WorkoutDay/workoutDay";
-import ClientList from "./components/Client-List/client-list";
-import { initializeApp } from "firebase/app";
-import {
-  getFirestore,
-  collection,
-  getDocs,
-  addDoc,
-  query,
-  where,
-  onSnapshot,
-  getDoc,
-  updateDoc,
-  doc,
-} from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBYwcfG5kErnzPDeDOjtIlE-g698jwltVM",
-  authDomain: "fitness-app-1e36b.firebaseapp.com",
-  projectId: "fitness-app-1e36b",
-  storageBucket: "fitness-app-1e36b.appspot.com",
-  messagingSenderId: "604488646893",
-  appId: "1:604488646893:web:521903a0c8db441b9a4ce4",
-  measurementId: "G-MG7P82E8BV",
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./routes/home";
+import EditProgram from "./routes/edit-program";
 
 function App() {
-  const [idSearchField, setIdSearchField] = React.useState("");
-  const [updateArr, setUpdateArr] = React.useState([]);
-
-  useEffect(() => {
-    let arr = [];
-    for (let i = 0; i < 7; i++) {
-      const dayObj = {
-        day: i + 1,
-        complete: false,
-        workout: [],
-      };
-      arr.push(dayObj);
-    }
-    setUpdateArr(arr);
-  }, []);
-
-  const handleChange = (event) => {
-    setIdSearchField(event.target.value);
-  };
-
-  const handleUpdateClient = async () => {
-    const docRef = doc(db, "users", idSearchField);
-    await updateDoc(docRef, {
-      workout: updateArr,
-    })
-      .then(() => alert("update successful"))
-      .catch((err) => alert(`unsucessful, error:${err}`));
-  };
-
-  const handleUpdateWeek = (dayArray, index) => {
-    const arrCopy = [...updateArr];
-    arrCopy[index] = { ...arrCopy[index], workout: dayArray };
-    setUpdateArr(arrCopy);
-  };
-
   return (
-    <div className="App">
-      <h2>Welcome Austin</h2>
-      <ClientList db={db} />
-      <input
-        type="text"
-        placeholder="user ID"
-        value={idSearchField}
-        onChange={handleChange}
-      />
-      {updateArr.map((day, index) => {
-        return (
-          <WorkoutDay dayIndex={index} handleUpdateWeek={handleUpdateWeek} />
-        );
-      })}
-      <button onClick={handleUpdateClient}>Update Weekly Plan to Client</button>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/edit" element={<EditProgram />} />
+    </Routes>
   );
 }
 
